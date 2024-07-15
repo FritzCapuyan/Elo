6f578297fe2a9616fffcd064950fd8cd69b35c19
Zack Young <102871747+FritzCapuyan@users.noreply.github.com>
2024-07-15 13:49:21 -0400

    Adding some comments

--- elo.py
+++ elo.py
@@ -14,7 +14,9 @@ def get_elo(competitor, elo_dict):
     return competitor_score, elo_dict
 
 
-def check_data(ids, results, extras):
+def check_data(ids, results, extras=None):
+    if extras is None:
+        extras = [0 for x in results]
     if not len(ids) == len(results) == len(extras):
         raise BaseException("Elements not the same length")
     return zip(ids, results, extras)
@@ -65,7 +67,21 @@ def fit_elos(elo_df, K_num, K, extra_factor, return_type = "dict"):
     else:
         return -loglike
 
-def elo(ids, results, extras, return_type='dict', params = None):
+def elo(ids, results, extras = None, return_type='dict', params = None):
+    """
+    ids: list of tuples of length 2 which represent unique ids of participants in the pairwise comparison
+
+    results: list of values between 0 and 1 representing the score of the first id in the associated id pair. For example,
+    the lists ids = [(1,2)] and results = [0] would mean that player 1 lost the first competition
+
+    extras: optional list that is a placeholder for adding extra parameters, such as home field advantage. 
+
+    return_type: one of 'dict', 'list', or 'log_loss'. Determines which results are returned. 'Dict' returns the dictionary with participant ids as keys
+    and scores as values
+
+    params: optional dictionary containing parameters to be used to fit elo score. Requires keys: 'K_num', 'K', 'extra'. The values supplied to 'K_num' 
+    and 'K' must be the same length
+    """
     elo_df = list(check_data(ids, results, extras))
     if params is None:
         res= minimize(lambda x: fit_elos(elo_df=elo_df, return_type='log_loss', K_num = [0, 5, 10, 15, 20], K=x[:-1], extra_factor=x[-1]), x0=[170, 170, 170, 170, 170, 50])
